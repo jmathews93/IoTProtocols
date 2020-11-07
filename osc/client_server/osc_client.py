61c75400c2e38ecee12ced63ebd98b0e5632b2f2
John G Thevos <admin@mindcette.com>
2020-11-07 12:42:15 -0500

    new osc client allows for custom IP address passed in via command line

--- osc/client_server/osc_client.py
+++ osc/client_server/osc_client.py
@@ -1,9 +1,13 @@
 from pythonosc.udp_client import SimpleUDPClient
+import sys
 
-ip = "127.0.0.1"
-port = 5555
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        ip = sys.argv[1]
+        print("IP: ", ip)
+        port = 5555
 
-client = SimpleUDPClient(ip, port)  # Create client
+        client = SimpleUDPClient(ip, port)  # Create client
 
-client.send_message("/some/address", 123)   # Send float message
-client.send_message("/some/address", [1, 2., "hello"])  # Send message with int, float and string
+        client.send_message("/some/address", 123)   # Send float message
+        client.send_message("/some/address", [1, 2., "hello"])  # Send message with int, float and string
