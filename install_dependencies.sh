cba95f6cde0399949bf225836085c3c24bb601b8
jmathews93 <mathews1@bu.edu>
2020-11-07 12:12:38 -0500

    a

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -11,15 +11,15 @@ echo "${PY_VER3:1:8}"
 if [ "${PY_VER:1:8}" = "python 3" ] 
 then
     echo "PIP"
-    # pip install paho-mqtt
-    # sudo pip install CoAPthon
-    # pip install python-osc
+    pip install paho-mqtt
+    sudo pip install CoAPthon
+    pip install python-osc
 elif [ "${PY_VER3:1:8}" = "python 3" ]
 then
     echo "PIP3"
-    # pip3 install paho-mqtt
-    # sudo pip3 install CoAPthon
-    # pip3 install python-osc
+    pip3 install paho-mqtt
+    sudo pip3 install CoAPthon
+    pip3 install python-osc
 fi
     
 
