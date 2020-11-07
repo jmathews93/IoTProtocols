8bfab68ee74a07e0884da1f5f1f845974fcfd31c
John G Thevos <admin@mindcette.com>
2020-11-07 12:49:58 -0500

    server now accepts custom IP

--- osc/client_server/osc_server.py
+++ osc/client_server/osc_server.py
@@ -1,6 +1,7 @@
 from pythonosc.dispatcher import Dispatcher
 from pythonosc.osc_server import BlockingOSCUDPServer
 
+import sys
 
 def print_handler(address, *args):
     print("{address}: {args}")
@@ -10,15 +11,18 @@ def default_handler(address, *args):
     print("DEFAULT {address}: {args}")
 
 
-dispatcher = Dispatcher()
-dispatcher.map("/something/*", print_handler)
-dispatcher.set_default_handler(default_handler)
 
-ip = "127.0.0.1" #localhost
-port = 5555
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        ip = sys.argv[1]
+        port = 5555
 
-print("Listening on port 5555...")
+        dispatcher = Dispatcher()
+        dispatcher.map("/something/*", print_handler)
+        dispatcher.set_default_handler(default_handler)
 
-server = BlockingOSCUDPServer((ip, port), dispatcher)
-server.serve_forever()  # Blocks forever
 
+        print("Listening on port 5555...")
+
+        server = BlockingOSCUDPServer((ip, port), dispatcher)
+        server.serve_forever()  # Blocks forever
