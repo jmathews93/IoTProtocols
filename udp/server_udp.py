f551f7bc5c80e30155b579a9e01e3bb77b0a1c9a
jmathews93 <mathews1@bu.edu>
2020-11-07 14:49:37 -0500

    removed spaces

--- udp/server_udp.py
+++ udp/server_udp.py
@@ -7,7 +7,6 @@ bufferSize  = 1024
 msgFromServer       = "Hello UDP Client"
 bytesToSend         = str.encode(msgFromServer)
 
- 
 
 # Create a datagram socket
 UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
@@ -18,7 +17,6 @@ UDPServerSocket.bind((localIP, localPort))
 print("UDP server up and listening")
 
 # Listen for incoming datagrams
-
 while(True):
 
     bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
