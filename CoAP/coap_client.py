b7facf773fc9d536249354f5e260f76ee9e47ea4
jmathews93 <mathews1@bu.edu>
2020-11-07 13:48:59 -0500

    added file capanilities to coap

--- CoAP/coap_client.py
+++ CoAP/coap_client.py
@@ -1,10 +1,21 @@
 from coapthon.client.helperclient import HelperClient
 
-host = "192.168.1.8"
-port = 5555
-path ="basic"
+import sys
 
-client = HelperClient(server=(host, port))
-response = client.get(path)
-print(response.pretty_print())
-client.stop()
+if __name__ == "__main__":
+
+    if len(sys.argv) > 1:
+        file_name = sys.argv[1]
+        print("file name: ", file_name)
+        file = open(file_name, "r")
+        json = file.read()
+        print("file contents: ", json)
+
+        host = "192.168.1.8"
+        port = 5555
+        path = "basic"
+
+        client = HelperClient(server=(host, port))
+        response = client.get(path)
+        print(response.pretty_print())
+        client.stop()
