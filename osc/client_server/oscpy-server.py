36a8d8e0b0c0849403beb0a2618a2fb8b1f87ca6
John-Anthony Thevos <john.thevos@gmail.com>
2020-11-22 21:28:31 -0500

    figured out max UDP datagram size

--- osc/client_server/oscpy-server.py
+++ osc/client_server/oscpy-server.py
@@ -1,11 +1,16 @@
 from oscpy.server import OSCThreadServer
 from time import sleep
 
-def callback(*values):
+def callback_message(*values):
     print(values)
 
-osc = OSCThreadServer()
+def callback_bundle(*values):
+    print("in bundle")
+    print(values)
+
+osc = OSCThreadServer(encoding='utf8')
 sock = osc.listen(address='192.168.1.85', port=5555, default=True)
-osc.bind(b'/ping', callback)
+osc.bind('/ping', callback_message)
+osc.bind(b'/pong', callback_bundle)
 sleep(1000)
 #osc.stop()
