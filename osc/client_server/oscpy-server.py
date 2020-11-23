050d5cf1bf5ea1a185c967eded0cf907faadaf04
jmathews93 <mathews1@bu.edu>
2020-11-23 14:20:52 -0500

    added command line args

--- osc/client_server/oscpy-server.py
+++ osc/client_server/oscpy-server.py
@@ -1,16 +1,26 @@
 from oscpy.server import OSCThreadServer
 from time import sleep
 
+
 def callback_message(*values):
     print(values)
+
 
 def callback_bundle(*values):
     print("in bundle")
     print(values)
 
-osc = OSCThreadServer(encoding='utf8')
-sock = osc.listen(address='192.168.1.85', port=5555, default=True)
-osc.bind('/ping', callback_message)
-osc.bind(b'/pong', callback_bundle)
-sleep(1000)
-#osc.stop()
+
+if __name__ == "__main__":
+    if len(sys.argv) > 0:
+        ip = sys.argv[1]
+        print("IP: ", ip)
+
+        osc = OSCThreadServer(encoding='utf8')
+        # sock = osc.listen(address='192.168.1.85', port=5555, default=True)
+        sock = osc.listen(address=ip, port=5555, default=True)
+
+        osc.bind('/ping', callback_message)
+        osc.bind(b'/pong', callback_bundle)
+        sleep(1000)
+        # osc.stop()
