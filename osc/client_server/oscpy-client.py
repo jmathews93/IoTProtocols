587f2d8fe4ca5af2679231f326b8109975122c6c
John-Anthony Thevos <john.thevos@gmail.com>
2020-11-22 20:10:32 -0500

    working OSC to rasperry pi over WAN

--- osc/client_server/oscpy-client.py
+++ osc/client_server/oscpy-client.py
@@ -1,5 +1,20 @@
 from oscpy.client import OSCClient
+import json, sys
 
-osc = OSCClient("172.25.27.69", 5555)
-for i in range(10):
-    osc.send_message(b'/ping', [i])
+osc = OSCClient("192.168.1.85", 5555, encoding='utf8')   # JA
+osc = OSCClient("73.131.93.124", 5555, encoding='utf8')  # Jared
+
+
+file = open('../../tides.json', 'r')
+x = file.read()
+print(sys.getsizeof(x))
+
+with open('../../tides.json') as f:
+  data = json.load(f)
+
+
+for i in range(len(data['allTides'])):
+    list = []
+    list.append(data['allTides'][i]['time'])
+    list.append(data['allTides'][i]['height'])
+    osc.send_message(b'/ping', list)
--- osc/client_server/oscpy-server.py
+++ osc/client_server/oscpy-server.py
@@ -1,13 +1,11 @@
 from oscpy.server import OSCThreadServer
 from time import sleep
 
-def callback(values):
-    print("got values: {}".format(values))
+def callback(*values):
+    print(values)
 
 osc = OSCThreadServer()
-sock = osc.listen(address='172.25.27.69', port=5555, default=True)
+sock = osc.listen(address='192.168.1.85', port=5555, default=True)
 osc.bind(b'/ping', callback)
 sleep(1000)
 #osc.stop()
-
-#from oscpy.server import OSCThreadServer
