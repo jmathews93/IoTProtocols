05d24382e158dbd447e7e8174c874646ede235f5
John-Anthony Thevos <john.thevos@gmail.com>
2020-12-01 13:17:16 -0500

    updated udp tests

--- udp/client_udp.py
+++ udp/client_udp.py
@@ -41,7 +41,7 @@ def test_case_1():
     Sends each recrord in the JSON file separately.
     """
 
-    for i in range(len(data['allTides'])):
+    for i in range(10000):
         list = []
         list.append(data['allTides'][i]['time'])
         list.append(data['allTides'][i]['height'])
@@ -56,7 +56,7 @@ def test_case_2():
     Sends the first 9203 characters in the string representation
     of the json file.
     """
-    for i in range(loop_len):
+    for i in range(10000):
         bytesToSend         = str.encode(jsn[0:9203])
         UDPClientSocket.sendto(bytesToSend, serverAddressPort)
 
@@ -66,33 +66,38 @@ def test_case_3():
     Sends the first 65000 characters in the string representation
     of the json file to simulate a max packet size.
     """
-    for i in range(loop_len):
+    for i in range(10000):
         bytesToSend         = str.encode(jsn[0:65000])
         UDPClientSocket.sendto(bytesToSend, serverAddressPort)
 
 def main():
-    # print "Test Case 1:"
-    # for i in range(30):
-    #     print "Trial", i, "\n"
-    #     test_case_1()
-    #     print "End Trial", i, "\n"
-    #     sleep(3)
-    # sleep(15)
-    # 
+    osc.send_message('/ping', [1], safer=True)
+    sleep(1)
+
+
+    print "Test Case 1:"
+    for i in range(num_tests):
+        print "Trial", i, "\n"
+        test_case_1()
+        print "End Trial", i, "\n"
+        sleep(0.2)
+
     # print "Test Case 2:"
-    # for i in range(30):
+    # for i in range(num_tests):
     #     print "Trial", i, ":"
     #     test_case_2()
     #     print "End Trial", i, "\n"
-    #     sleep(3)
-    # sleep(15)
-    #
-    print "Test Case 3"
-    for i in range(30):
-        print "Trial", i, ":"
-        test_case_3()
-        print "End Trial", i, "\n"
-        sleep(3)
+    #     sleep(0.2)
+    # #
+    # print "Test Case 3"
+    # for i in range(num_tests):
+    #     print "Trial", i, ":"
+    #     test_case_3()
+    #     print "End Trial", i, "\n"
+    #     sleep(0.2)
+
+
+    osc.send_message('/ping', [1], safer=True)
 
 
 if __name__ == "__main__":
