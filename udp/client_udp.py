719eec4a4e6224d855fb9db76b4fff0b87a7d79c
John-Anthony Thevos <john.thevos@gmail.com>
2020-12-01 13:22:59 -0500

    hatred.

--- udp/client_udp.py
+++ udp/client_udp.py
@@ -77,21 +77,21 @@ def main():
 
 
     print "Test Case 1:"
-    for i in range(num_tests):
+    for i in range(30):
         print "Trial", i, "\n"
         test_case_1()
         print "End Trial", i, "\n"
         sleep(0.2)
 
     # print "Test Case 2:"
-    # for i in range(num_tests):
+    # for i in range(30):
     #     print "Trial", i, ":"
     #     test_case_2()
     #     print "End Trial", i, "\n"
     #     sleep(0.2)
     # #
     # print "Test Case 3"
-    # for i in range(num_tests):
+    # for i in range(30):
     #     print "Trial", i, ":"
     #     test_case_3()
     #     print "End Trial", i, "\n"
