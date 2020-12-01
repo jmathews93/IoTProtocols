2162c71ded1ba4100d408cebe0e27244d19117d8
jmathews93 <mathews1@bu.edu>
2020-11-30 23:29:45 -0500

    all averages are calculated

---
+++ graphs/generate_graphs.py
@@ -0,0 +1,208 @@
+#Graphs needed:
+
+# Protocol 	Test 	Avg Packets/s 	Avg Time 	Total Time 
+# UDP Sending 	1 	29335.00 	1.40 	42
+# UDP Sending 	2 	13855.03 	2.57 	77
+# UDP Sending 	3 	11619.53 	10.00 	300
+				
+# OSC Sending 	1 	29275.93 	2.73 	82
+# OSC Sending 	2 	16912.93 	3.03 	91
+# OSC Sending 	3 	6142.73 	5.77 	173
+				
+# OSC Receiving 	1 	29333.63 	10.57 	317
+				
+# OSC Receiving Pi 	1 	8427.53 	2.87 	86
+# OSC Receiving Pi 	2 	3117.97 	3.17 	96
+# OSC Receiving Pi 	3 	634.20 	5.87 	176
+
+import csv, sys
+
+
+#osc wan stats
+def avg_packets_per_s(file_name):
+    sum = 0
+
+    with open(file_name, newline='') as f:
+            reader = csv.reader(f)
+            data = list(reader)
+
+    for i in range(1, len(data)):
+        sum = sum + int(data[i][1])
+
+    avg = sum/30
+
+    return avg
+
+def avg_time():
+    pass
+
+def total_time():
+    pass
+
+
+def main():
+    #region OSC Stats
+    ##################################################################################################################
+    #OSC WAN Pi Recv
+    osc_wan_recv_pi_test1_avg = avg_packets_per_s('../results/cleaned_data/osc/wan/receiving/test_1_30_trials_receiving_pi.csv')
+    print("OSC WAN Rcv pi Test1: " + str(osc_wan_recv_pi_test1_avg))
+    osc_wan_recv_pi_test2_avg = avg_packets_per_s('../results/cleaned_data/osc/wan/receiving/test_2_30_trials_receiving_pi.csv')
+    print("OSC WAN Rcv pi Test2: " + str(osc_wan_recv_pi_test2_avg))
+    osc_wan_recv_pi_test3_avg = avg_packets_per_s('../results/cleaned_data/osc/wan/receiving/test_3_30_trials_receiving_pi.csv')
+    print("OSC WAN Rcv pi Test3: " + str(osc_wan_recv_pi_test3_avg))
+
+    #OSC WAN Pi Send
+    osc_wan_send_pi_test1_avg = avg_packets_per_s('../results/cleaned_data/osc/wan/sending/test_1_30_trials_sending_pi.csv')
+    print("OSC WAN Send pi Test1: " + str(osc_wan_send_pi_test1_avg))
+
+    #OSC WAN PC Recv
+    osc_wan_recv_pc_test1_avg = avg_packets_per_s('../results/cleaned_data/osc/wan/receiving/test_1_30_trials_recieving_jgtmac.csv')
+    print("OSC WAN Rcv PC Test1: " + str(osc_wan_recv_pc_test1_avg))
+
+    #OSC WAN PC Send
+    osc_wan_send_pc_test1_avg = avg_packets_per_s('../results/cleaned_data/osc/wan/sending/test_1_30_trials_sending_jgtmac.csv')
+    print("OSC WAN Send PC Test1: " + str(osc_wan_send_pc_test1_avg))
+    osc_wan_send_pc_test2_avg = avg_packets_per_s('../results/cleaned_data/osc/wan/sending/test_2_30_trials_sending_jgtmac.csv')
+    print("OSC WAN Send PC Test2: " + str(osc_wan_send_pc_test2_avg))
+    osc_wan_send_pc_test3_avg = avg_packets_per_s('../results/cleaned_data/osc/wan/sending/test_3_30_trials_sending_jgtmac.csv')
+    print("OSC WAN Send PC Test3: " + str(osc_wan_send_pc_test3_avg))
+
+    #OSC LAN
+    #################
+    #OSC LAN Pi Recv
+    osc_lan_recv_pi_test1_avg = avg_packets_per_s('../results/cleaned_data/osc/lan/trial1/lan_test_1_receiving_pi.csv')
+    print("OSC LAN Rcv pi Test1: " + str(osc_lan_recv_pi_test1_avg))
+    osc_lan_recv_pi_test2_avg = avg_packets_per_s('../results/cleaned_data/osc/lan/trial2/lan_test_2_receiving_pi.csv')
+    print("OSC LAN Rcv pi Test2: " + str(osc_lan_recv_pi_test2_avg))
+    osc_lan_recv_pi_test3_avg = avg_packets_per_s('../results/cleaned_data/osc/lan/trial3/lan_test_3_receiving_pi.csv')
+    print("OSC LAN Rcv pi Test3: " + str(osc_lan_recv_pi_test3_avg))
+
+    #OSC LAN Pi Send
+    osc_lan_send_pi_test1_avg = avg_packets_per_s('../results/cleaned_data/osc/lan/trial1/lan_test_1_sending_pi.csv')
+    print("OSC LAN Rcv pi Test1: " + str(osc_lan_send_pi_test1_avg))
+    osc_lan_send_pi_test2_avg = avg_packets_per_s('../results/cleaned_data/osc/lan/trial2/lan_test_2_sending_pi.csv')
+    print("OSC LAN Rcv pi Test2: " + str(osc_lan_send_pi_test2_avg))
+    osc_lan_send_pi_test3_avg = avg_packets_per_s('../results/cleaned_data/osc/lan/trial3/lan_test_3_sending_pi.csv')
+    print("OSC LAN Rcv pi Test3: " + str(osc_lan_send_pi_test3_avg))
+
+    #OSC LAN PC Recv
+    osc_lan_recv_pc_test1_avg = avg_packets_per_s('../results/cleaned_data/osc/lan/trial1/lan_test_1_receiving_jamMac.csv')
+    print("OSC LAN Rcv PC Test1: " + str(osc_lan_recv_pc_test1_avg))
+    osc_lan_recv_pc_test2_avg = avg_packets_per_s('../results/cleaned_data/osc/lan/trial2/lan_test_2_receiving_jamMac.csv')
+    print("OSC LAN Rcv PC Test2: " + str(osc_lan_recv_pc_test2_avg))
+    osc_lan_recv_pc_test3_avg = avg_packets_per_s('../results/cleaned_data/osc/lan/trial3/lan_test_3_receiving_jamMac.csv')
+    print("OSC LAN Rcv PC Test3: " + str(osc_lan_recv_pc_test3_avg))
+
+    #OSC LAN PC Send
+    osc_lan_send_pc_test1_avg = avg_packets_per_s('../results/cleaned_data/osc/lan/trial1/lan_test_1_sending_jamMac.csv')
+    print("OSC LAN Send PC Test1: " + str(osc_lan_send_pc_test1_avg))
+    osc_lan_send_pc_test2_avg = avg_packets_per_s('../results/cleaned_data/osc/lan/trial2/lan_test_2_sending_jamMac.csv')
+    print("OSC LAN Send PC Test2: " + str(osc_lan_send_pc_test2_avg))
+    osc_lan_send_pc_test3_avg = avg_packets_per_s('../results/cleaned_data/osc/lan/trial3/lan_test_3_sending_jamMac.csv')
+    print("OSC LAN Send PC Test3: " + str(osc_lan_send_pc_test3_avg))
+
+    #endregion
+    ##################################################################################################################
+    
+    #region UDP Stats
+    #######################################################################################################################################################
+    #UDP WAN Pi Recv
+    udp_wan_recv_pi_test1_avg = avg_packets_per_s('../results/cleaned_data/udp/wan/trial1/udp_test_1_30_trials_receiving_pi.csv')
+    print("OSC WAN Rcv pi Test1: " + str(udp_wan_recv_pi_test1_avg))
+    udp_wan_recv_pi_test2_avg = avg_packets_per_s('../results/cleaned_data/udp/wan/trial2/udp_test_2_30_trials_receiving_pi.csv')
+    print("OSC WAN Rcv pi Test2: " + str(udp_wan_recv_pi_test2_avg))
+    udp_wan_recv_pi_test3_avg = avg_packets_per_s('../results/cleaned_data/udp/wan/trial3/udp_test_3_30_trials_receiving_pi.csv')
+    print("OSC WAN Rcv pi Test3: " + str(udp_wan_recv_pi_test3_avg))
+
+    #UDP WAN Pc Send
+    udp_wan_send_pc_test1_avg = avg_packets_per_s('../results/cleaned_data/udp/wan/trial1/udp_test_1_30_trials_sending_jgtmac.csv')
+    print("OSC WAN send pc Test1: " + str(udp_wan_send_pc_test1_avg))
+    udp_wan_send_pc_test2_avg = avg_packets_per_s('../results/cleaned_data/udp/wan/trial2/udp_test_2_30_trials_sending_jgtmac.csv')
+    print("OSC WAN send pc Test2: " + str(udp_wan_send_pc_test2_avg))
+    udp_wan_send_pc_test3_avg = avg_packets_per_s('../results/cleaned_data/udp/wan/trial3/udp_test_3_30_trials_sending_jgtmac.csv')
+    print("OSC WAN send pc Test3: " + str(udp_wan_send_pc_test3_avg))
+
+    #UDP LAN
+    ###################################
+    #UDP LAN Pi Recv
+    udp_lan_recv_pi_test1_avg = avg_packets_per_s('../results/cleaned_data/udp/lan/trial1/udp_lan_test_1_receiving_pi.csv')
+    print("UDP LAN Rcv pi Test1: " + str(udp_lan_recv_pi_test1_avg))
+    udp_lan_recv_pi_test2_avg = avg_packets_per_s('../results/cleaned_data/udp/lan/trial2/udp_lan_test_2_receiving_pi.csv')
+    print("UDP LAN Rcv pi Test2: " + str(udp_lan_recv_pi_test2_avg))
+    udp_lan_recv_pi_test3_avg = avg_packets_per_s('../results/cleaned_data/udp/lan/trial3/udp_lan_test_3_receiving_pi.csv')
+    print("UDP LAN Rcv pi Test3: " + str(udp_lan_recv_pi_test3_avg))
+
+    #UDP LAN Pi Send
+    udp_lan_send_pi_test1_avg = avg_packets_per_s('../results/cleaned_data/udp/lan/trial1/udp_lan_test_1_sending_pi.csv')
+    print("UDP LAN Send pi Test1: " + str(udp_lan_send_pi_test1_avg))
+    udp_lan_send_pi_test2_avg = avg_packets_per_s('../results/cleaned_data/udp/lan/trial2/udp_lan_test_2_sending_pi.csv')
+    print("UDP LAN Send pi Test2: " + str(udp_lan_send_pi_test2_avg))
+    udp_lan_send_pi_test3_avg = avg_packets_per_s('../results/cleaned_data/udp/lan/trial3/udp_lan_test_3_sending_pi.csv')
+    print("UDP LAN Send pi Test3: " + str(udp_lan_send_pi_test3_avg))
+
+    #UDP LAN PC Recv
+    udp_lan_recv_pc_test1_avg = avg_packets_per_s('../results/cleaned_data/udp/lan/trial1/udp_lan_test_1_receiving_jamMac.csv')
+    print("UDP LAN Rcv pc Test1: " + str(udp_lan_recv_pc_test1_avg))
+    udp_lan_recv_pc_test2_avg = avg_packets_per_s('../results/cleaned_data/udp/lan/trial2/udp_lan_test_2_receiving_jamMac.csv')
+    print("UDP LAN Rcv pc Test2: " + str(udp_lan_recv_pc_test2_avg))
+    udp_lan_recv_pc_test3_avg = avg_packets_per_s('../results/cleaned_data/udp/lan/trial3/udp_lan_test_3_receiving_jamMac.csv')
+    print("UDP LAN Rcv pc Test3: " + str(udp_lan_recv_pc_test3_avg))
+
+    #UDP LAN PC Send
+    udp_lan_send_pc_test1_avg = avg_packets_per_s('../results/cleaned_data/udp/lan/trial1/udp_lan_test_1_sending_jamMac.csv')
+    print("UDP LAN Send pc Test1: " + str(udp_lan_send_pc_test1_avg))
+    udp_lan_send_pc_test2_avg = avg_packets_per_s('../results/cleaned_data/udp/lan/trial2/udp_lan_test_2_sending_jamMac.csv')
+    print("UDP LAN Send pc Test2: " + str(udp_lan_send_pc_test2_avg))
+    udp_lan_send_pc_test3_avg = avg_packets_per_s('../results/cleaned_data/udp/lan/trial3/udp_lan_test_3_sending_jamMac.csv')
+    print("Udp LAN Send pc Test3: " + str(udp_lan_send_pc_test3_avg))
+
+    #endregion
+
+    #region COAP Stats
+    #######################################################################################################################################################
+    #COap WAN Pi Recv
+    coap_wan_recv_pi_test1_avg = avg_packets_per_s('../results/cleaned_data/coap/wan/wancoap_test_1_30_trials_sending_pi.csv')
+    print("Coap WAN Rcv pi Test1: " + str(coap_wan_recv_pi_test1_avg * 30))
+
+    #COap LAN Pi Recv
+    coap_lan_recv_pi_test1_avg = avg_packets_per_s('../results/cleaned_data/coap/lan/trial1/coap_lan_test1_receiving_pi_u.csv')
+    print("COAP LAN Rcv pi Test1: " + str(coap_lan_recv_pi_test1_avg * 30))
+    coap_lan_recv_pi_test2_avg = avg_packets_per_s('../results/cleaned_data/coap/lan/trial2/coap_lan_trial_receiving_pi.csv')
+    print("COAP LAN Rcv pi Test2: " + str(coap_lan_recv_pi_test2_avg * 30))
+    coap_lan_recv_pi_test3_avg = avg_packets_per_s('../results/cleaned_data/coap/lan/trial3/coap_trial3_receiving_pi.csv')
+    print("COAP LAN Rcv pi Test3: " + str(coap_lan_recv_pi_test3_avg * 30))
+
+    #COap LAN Pi Send
+    coap_lan_send_pi_test1_avg = avg_packets_per_s('../results/cleaned_data/coap/lan/trial1/coap_lan_test1_sending_piu.csv')
+    print("COAP LAN Send pi Test1: " + str(coap_lan_send_pi_test1_avg * 30))
+    coap_lan_send_pi_test2_avg = avg_packets_per_s('../results/cleaned_data/coap/lan/trial2/coap_lan_tria2_sending_pi.csv')
+    print("COAP LAN Send pi Test2: " + str(coap_lan_send_pi_test2_avg * 30))
+    coap_lan_send_pi_test3_avg = avg_packets_per_s('../results/cleaned_data/coap/lan/trial3/coap_trial3_sending_pi.csv')
+    print("COAP LAN Send pi Test3: " + str(coap_lan_send_pi_test3_avg * 30))
+
+    #COap LAN PC Send
+    coap_lan_send_pc_test1_avg = avg_packets_per_s('../results/cleaned_data/coap/lan/trial1/coap_lan_test1_seding_jamU.csv')
+    print("COAP LAN Send pc Test1: " + str(coap_lan_send_pc_test1_avg * 30))
+    coap_lan_send_pc_test2_avg = avg_packets_per_s('../results/cleaned_data/coap/lan/trial2/coap_lan_trial2_sending_jam.csv')
+    print("COAP LAN Send pc Test2: " + str(coap_lan_send_pc_test2_avg * 30))
+    coap_lan_send_pc_test3_avg = avg_packets_per_s('../results/cleaned_data/coap/lan/trial3/coap_trial3_sending_jam.csv')
+    print("COAP LAN Send pc Test3: " + str(coap_lan_send_pc_test3_avg * 30))
+
+    #COap LAN PC Recv
+    coap_lan_recv_pc_test1_avg = avg_packets_per_s('../results/cleaned_data/coap/lan/trial1/coap_lan_test1_receiving_jamu.csv')
+    print("COAP LAN Rcv pc Test1: " + str(coap_lan_recv_pc_test1_avg * 30))
+    coap_lan_recv_pc_test2_avg = avg_packets_per_s('../results/cleaned_data/coap/lan/trial2/coap_lan_trial2_receiving_jam.csv')
+    print("COAP LAN Rcv pc Test2: " + str(coap_lan_recv_pc_test2_avg * 30))
+    coap_lan_recv_pc_test3_avg = avg_packets_per_s('../results/cleaned_data/coap/lan/trial3/coap_trial3_receiving_jam.csv')
+    print("COAP LAN Rcv pc Test3: " + str(coap_lan_recv_pc_test3_avg * 30))
+    #endregion
+    
+    ##################################################################################################################
+    #GRAPHS
+    #############
+
+
+
+
+if __name__ == '__main__':
+    main()
--- graphs/run.sh
+++ graphs/run.sh
@@ -4,67 +4,67 @@
 #############
 
 # CoAP LAN Trial1
-python3 cleanData.py ../results/coap/lan/trial1/coap_lan_test1_receiving_jamu.txt ../results/cleaned_data/coap/lan/trial1/
-python3 cleanData.py ../results/coap/lan/trial1/coap_lan_test1_receiving_pi_u.txt ../results/cleaned_data/coap/lan/trial1/
-python3 cleanData.py ../results/coap/lan/trial1/coap_lan_test1_seding_jamU.txt ../results/cleaned_data/coap/lan/trial1/
-python3 cleanData.py ../results/coap/lan/trial1/coap_lan_test1_sending_piu.txt ../results/cleaned_data/coap/lan/trial1/
-python3 cleanData.py ../results/coap/lan/trial1/coap_lan_test2_receiving_pi.txt ../results/cleaned_data/coap/lan/trial1/
-python3 cleanData.py ../results/coap/lan/trial1/coap_lan_test2_receiving_pi.txt ../results/cleaned_data/coap/lan/trial1/
+# python3 cleanData.py ../results/coap/lan/trial1/coap_lan_test1_receiving_jamu.txt ../results/cleaned_data/coap/lan/trial1/
+# python3 cleanData.py ../results/coap/lan/trial1/coap_lan_test1_receiving_pi_u.txt ../results/cleaned_data/coap/lan/trial1/
+# python3 cleanData.py ../results/coap/lan/trial1/coap_lan_test1_seding_jamU.txt ../results/cleaned_data/coap/lan/trial1/
+# python3 cleanData.py ../results/coap/lan/trial1/coap_lan_test1_sending_piu.txt ../results/cleaned_data/coap/lan/trial1/
+# python3 cleanData.py ../results/coap/lan/trial1/coap_lan_test2_receiving_pi.txt ../results/cleaned_data/coap/lan/trial1/
+# python3 cleanData.py ../results/coap/lan/trial1/coap_lan_test2_receiving_pi.txt ../results/cleaned_data/coap/lan/trial1/
 
-#CoAP LAN Trial2
-python3 cleanData.py ../results/coap/lan/trial2/coap_lan_tria2_sending_pi.txt ../results/cleaned_data/coap/lan/trial2/
-python3 cleanData.py ../results/coap/lan/trial2/coap_lan_trial2_receiving_jam.txt ../results/cleaned_data/coap/lan/trial2/
-python3 cleanData.py ../results/coap/lan/trial2/coap_lan_trial2_sending_jam.txt ../results/cleaned_data/coap/lan/trial2/
-python3 cleanData.py ../results/coap/lan/trial2/coap_lan_trial_receiving_pi.txt ../results/cleaned_data/coap/lan/trial2/
+# #CoAP LAN Trial2
+# python3 cleanData.py ../results/coap/lan/trial2/coap_lan_tria2_sending_pi.txt ../results/cleaned_data/coap/lan/trial2/
+# python3 cleanData.py ../results/coap/lan/trial2/coap_lan_trial2_receiving_jam.txt ../results/cleaned_data/coap/lan/trial2/
+# python3 cleanData.py ../results/coap/lan/trial2/coap_lan_trial2_sending_jam.txt ../results/cleaned_data/coap/lan/trial2/
+# python3 cleanData.py ../results/coap/lan/trial2/coap_lan_trial_receiving_pi.txt ../results/cleaned_data/coap/lan/trial2/
 
-#CoAP LAN Trial3
-python3 cleanData.py ../results/coap/lan/trial3/coap_trial3_receiving_jam.txt ../results/cleaned_data/coap/lan/trial3/
-python3 cleanData.py ../results/coap/lan/trial3/coap_trial3_receiving_pi.txt ../results/cleaned_data/coap/lan/trial3/
-python3 cleanData.py ../results/coap/lan/trial3/coap_trial3_sending_jam.txt  ../results/cleaned_data/coap/lan/trial3/
-python3 cleanData.py ../results/coap/lan/trial3/coap_trial3_sending_pi.txt ../results/cleaned_data/coap/lan/trial3/
+# #CoAP LAN Trial3
+# python3 cleanData.py ../results/coap/lan/trial3/coap_trial3_receiving_jam.txt ../results/cleaned_data/coap/lan/trial3/
+# python3 cleanData.py ../results/coap/lan/trial3/coap_trial3_receiving_pi.txt ../results/cleaned_data/coap/lan/trial3/
+# python3 cleanData.py ../results/coap/lan/trial3/coap_trial3_sending_jam.txt  ../results/cleaned_data/coap/lan/trial3/
+# python3 cleanData.py ../results/coap/lan/trial3/coap_trial3_sending_pi.txt ../results/cleaned_data/coap/lan/trial3/
 
-UDP
-#############
-#UDP WAN
+# UDP
+# #############
+# #UDP WAN
 
-#TRIAL 1
-python3 cleanData.py ../results/udp/udp_test_1_30_trials_receiving_pi.txt ../results/cleaned_data/udp/wan/trial1/
-python3 cleanData.py ../results/udp/udp_test_1_30_trials_sending_jgtmac.txt ../results/cleaned_data/udp/wan/trial1/
+# #TRIAL 1
+# python3 cleanData.py ../results/udp/udp_test_1_30_trials_receiving_pi.txt ../results/cleaned_data/udp/wan/trial1/
+# python3 cleanData.py ../results/udp/udp_test_1_30_trials_sending_jgtmac.txt ../results/cleaned_data/udp/wan/trial1/
 
-#TRIAL 2
-python3 cleanData.py ../results/udp/udp_test_2_30_trials_receiving_pi.txt ../results/cleaned_data/udp/wan/trial2/
-python3 cleanData.py ../results/udp/udp_test_2_30_trials_sending_jgtmac.txt ../results/cleaned_data/udp/wan/trial2/
+# #TRIAL 2
+# python3 cleanData.py ../results/udp/udp_test_2_30_trials_receiving_pi.txt ../results/cleaned_data/udp/wan/trial2/
+# python3 cleanData.py ../results/udp/udp_test_2_30_trials_sending_jgtmac.txt ../results/cleaned_data/udp/wan/trial2/
 
-#TRIAL 3
-python3 cleanData.py ../results/udp/udp_test_3_30_trials_sending_jgtmac.txt ../results/cleaned_data/udp/wan/trial3/
-python3 cleanData.py ../results/udp/udp_test_3_30_trials_receiving_pi.txt ../results/cleaned_data/udp/wan/trial3/
+# #TRIAL 3
+# python3 cleanData.py ../results/udp/udp_test_3_30_trials_sending_jgtmac.txt ../results/cleaned_data/udp/wan/trial3/
+# python3 cleanData.py ../results/udp/udp_test_3_30_trials_receiving_pi.txt ../results/cleaned_data/udp/wan/trial3/
 
 
-UDP LAN
-#TRIAL 1
-python3 cleanData.py ../results/udp/lan/test1/udp_lan_test_1_receiving_jamMac.txt ../results/cleaned_data/udp/lan/trial1/
-python3 cleanData.py ../results/udp/lan/test1/udp_lan_test_1_receiving_pi.txt  ../results/cleaned_data/udp/lan/trial1/
-python3 cleanData.py ../results/udp/lan/test1/udp_lan_test_1_sending_jamMac.txt ../results/cleaned_data/udp/lan/trial1/
-python3 cleanData.py ../results/udp/lan/test1/udp_lan_test_1_sending_pi.txt ../results/cleaned_data/udp/lan/trial1/
+# UDP LAN
+# #TRIAL 1
+# python3 cleanData.py ../results/udp/lan/test1/udp_lan_test_1_receiving_jamMac.txt ../results/cleaned_data/udp/lan/trial1/
+# python3 cleanData.py ../results/udp/lan/test1/udp_lan_test_1_receiving_pi.txt  ../results/cleaned_data/udp/lan/trial1/
+# python3 cleanData.py ../results/udp/lan/test1/udp_lan_test_1_sending_jamMac.txt ../results/cleaned_data/udp/lan/trial1/
+# python3 cleanData.py ../results/udp/lan/test1/udp_lan_test_1_sending_pi.txt ../results/cleaned_data/udp/lan/trial1/
    
-#TRIAL 2
-python3 cleanData.py ../results/udp/lan/test2/udp_lan_test_2_receiving_jamMac.txt ../results/cleaned_data/udp/lan/trial2/
-python3 cleanData.py ../results/udp/lan/test2/udp_lan_test_2_receiving_pi.txt  ../results/cleaned_data/udp/lan/trial2/
-python3 cleanData.py ../results/udp/lan/test2/udp_lan_test_2_sending_jamMac.txt ../results/cleaned_data/udp/lan/trial2/
-python3 cleanData.py ../results/udp/lan/test2/udp_lan_test_2_sending_pi.txt ../results/cleaned_data/udp/lan/trial2/
+# #TRIAL 2
+# python3 cleanData.py ../results/udp/lan/test2/udp_lan_test_2_receiving_jamMac.txt ../results/cleaned_data/udp/lan/trial2/
+# python3 cleanData.py ../results/udp/lan/test2/udp_lan_test_2_receiving_pi.txt  ../results/cleaned_data/udp/lan/trial2/
+# python3 cleanData.py ../results/udp/lan/test2/udp_lan_test_2_sending_jamMac.txt ../results/cleaned_data/udp/lan/trial2/
+# python3 cleanData.py ../results/udp/lan/test2/udp_lan_test_2_sending_pi.txt ../results/cleaned_data/udp/lan/trial2/
 
 
-#TRIAL 3
-python3 cleanData.py ../results/udp/lan/test3/udp_lan_test_3_receiving_jamMac.txt ../results/cleaned_data/udp/lan/trial3/
-python3 cleanData.py ../results/udp/lan/test3/udp_lan_test_3_receiving_pi.txt  ../results/cleaned_data/udp/lan/trial3/
-python3 cleanData.py ../results/udp/lan/test3/udp_lan_test_3_sending_jamMac.txt ../results/cleaned_data/udp/lan/trial3/
-python3 cleanData.py ../results/udp/lan/test3/udp_lan_test_3_sending_pi.txt ../results/cleaned_data/udp/lan/trial3/
+# #TRIAL 3
+# python3 cleanData.py ../results/udp/lan/test3/udp_lan_test_3_receiving_jamMac.txt ../results/cleaned_data/udp/lan/trial3/
+# python3 cleanData.py ../results/udp/lan/test3/udp_lan_test_3_receiving_pi.txt  ../results/cleaned_data/udp/lan/trial3/
+# python3 cleanData.py ../results/udp/lan/test3/udp_lan_test_3_sending_jamMac.txt ../results/cleaned_data/udp/lan/trial3/
+# python3 cleanData.py ../results/udp/lan/test3/udp_lan_test_3_sending_pi.txt ../results/cleaned_data/udp/lan/trial3/
 
 
-OSC
+#OSC
 #############
 
-OSC WAN
+#OSC WAN
 
 #RANDOM
 python3 cleanData.py ../results/osc/test_1_30_trials_sending_pi.txt ../results/cleaned_data/osc/wan/random/
@@ -79,32 +79,32 @@ python3 cleanData.py ../results/osc/test_3_30_trials_receiving_pi.txt ../results
    
 
 #Receiving
-python3 cleanData.py ../results/osc/wan/receiving/osc_wan_recieving_test1.txt ../results/cleaned_data/osc/wan/receiving/
+# python3 cleanData.py ../results/osc/wan/receiving/osc_wan_recieving_test1.txt ../results/cleaned_data/osc/wan/receiving/
 
-#Sending
-python3 cleanData.py ../results/osc/wan/sending/osc_wan_tests-semisequential_1.txt ../results/cleaned_data/osc/wan/sending/
-python3 cleanData.py ../results/osc/wan/sending/osc_wan_tests-semisequential_2.txt ../results/cleaned_data/osc/wan/sending/
-python3 cleanData.py ../results/osc/wan/sending/osc_wan_tests-semisequential_3.txt ../results/cleaned_data/osc/wan/sending/
+# #Sending
+# python3 cleanData.py ../results/osc/wan/sending/osc_wan_tests-semisequential_1.txt ../results/cleaned_data/osc/wan/sending/
+# python3 cleanData.py ../results/osc/wan/sending/osc_wan_tests-semisequential_2.txt ../results/cleaned_data/osc/wan/sending/
+# python3 cleanData.py ../results/osc/wan/sending/osc_wan_tests-semisequential_3.txt ../results/cleaned_data/osc/wan/sending/
 
 
-#LAN
-#TRIAL1
-python3 cleanData.py ../results/osc/lan/test1/lan_test_1_receiving_jamMac.txt ../results/cleaned_data/osc/lan/trial1/
-python3 cleanData.py ../results/osc/lan/test1/lan_test_1_receiving_pi.txt ../results/cleaned_data/osc/lan/trial1/
-python3 cleanData.py ../results/osc/lan/test1/lan_test_1_sending_jamMac.txt ../results/cleaned_data/osc/lan/trial1/
-python3 cleanData.py ../results/osc/lan/test1/lan_test_1_sending_pi.txt ../results/cleaned_data/osc/lan/trial1/
+# #LAN
+# #TRIAL1
+# python3 cleanData.py ../results/osc/lan/test1/lan_test_1_receiving_jamMac.txt ../results/cleaned_data/osc/lan/trial1/
+# python3 cleanData.py ../results/osc/lan/test1/lan_test_1_receiving_pi.txt ../results/cleaned_data/osc/lan/trial1/
+# python3 cleanData.py ../results/osc/lan/test1/lan_test_1_sending_jamMac.txt ../results/cleaned_data/osc/lan/trial1/
+# python3 cleanData.py ../results/osc/lan/test1/lan_test_1_sending_pi.txt ../results/cleaned_data/osc/lan/trial1/
 
-#TRIAL2
-python3 cleanData.py ../results/osc/lan/test2/lan_test_2_receiving_jamMac.txt ../results/cleaned_data/osc/lan/trial2/
-python3 cleanData.py ../results/osc/lan/test2/lan_test_2_receiving_pi.txt ../results/cleaned_data/osc/lan/trial2/
-python3 cleanData.py ../results/osc/lan/test2/lan_test_2_sending_jamMac.txt ../results/cleaned_data/osc/lan/trial2/
-python3 cleanData.py ../results/osc/lan/test2/lan_test_2_sending_pi.txt ../results/cleaned_data/osc/lan/trial2/
+# #TRIAL2
+# python3 cleanData.py ../results/osc/lan/test2/lan_test_2_receiving_jamMac.txt ../results/cleaned_data/osc/lan/trial2/
+# python3 cleanData.py ../results/osc/lan/test2/lan_test_2_receiving_pi.txt ../results/cleaned_data/osc/lan/trial2/
+# python3 cleanData.py ../results/osc/lan/test2/lan_test_2_sending_jamMac.txt ../results/cleaned_data/osc/lan/trial2/
+# python3 cleanData.py ../results/osc/lan/test2/lan_test_2_sending_pi.txt ../results/cleaned_data/osc/lan/trial2/
 
-#TRIAL3
-python3 cleanData.py ../results/osc/lan/test3/lan_test_3_receiving_jamMac.txt ../results/cleaned_data/osc/lan/trial3/
-python3 cleanData.py ../results/osc/lan/test3/lan_test_3_receiving_pi.txt ../results/cleaned_data/osc/lan/trial3/
-python3 cleanData.py ../results/osc/lan/test3/lan_test_3_sending_jamMac.txt ../results/cleaned_data/osc/lan/trial3/
-python3 cleanData.py ../results/osc/lan/test3/lan_test_3_sending_pi.txt ../results/cleaned_data/osc/lan/trial3/
+# #TRIAL3
+# python3 cleanData.py ../results/osc/lan/test3/lan_test_3_receiving_jamMac.txt ../results/cleaned_data/osc/lan/trial3/
+# python3 cleanData.py ../results/osc/lan/test3/lan_test_3_receiving_pi.txt ../results/cleaned_data/osc/lan/trial3/
+# python3 cleanData.py ../results/osc/lan/test3/lan_test_3_sending_jamMac.txt ../results/cleaned_data/osc/lan/trial3/
+# python3 cleanData.py ../results/osc/lan/test3/lan_test_3_sending_pi.txt ../results/cleaned_data/osc/lan/trial3/
 
          
 
